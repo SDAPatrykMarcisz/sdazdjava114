4c83341292490e86a4d0b924bd31eab2420e64b2
Patryk <patryk.marcisz.v2@gmail.com>
2022-10-12 18:08:56 +0200

    initial repo

---
+++ src/main/java/pl/marcisz/patryk/zdjava114/Zdjava114Application.java
@@ -0,0 +1,13 @@
+package pl.marcisz.patryk.zdjava114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zdjava114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zdjava114Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marcisz/patryk/zdjava114/Zdjava114ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcisz.patryk.zdjava114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zdjava114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
